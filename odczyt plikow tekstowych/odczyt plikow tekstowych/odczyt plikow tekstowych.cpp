5fa1a33dcbdf82012bc9e236ac289dc0dd3615b9
Psujex24 <70110209+psujex24@users.noreply.github.com>
2024-05-20 00:07:41 +0200

    open

---
+++ odczyt plikow tekstowych/odczyt plikow tekstowych/odczyt plikow tekstowych.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+string imie, nazwisko;
+int nr_tel;
+
+int main()
+{
+	string linia;
+	int nr_linii = 1;
+
+	fstream plik;
+	plik.open("wizytowka.txt", ios::in);
+
+	if (plik.good() == false) cout << "Plik nie istnieje!";
+	
+
+	while (getline(plik, linia))
+	{
+		switch (nr_linii)
+		{
+			case 1: imie = linia; break;
+			case 2: nazwisko = linia; break;
+			case 3: nr_tel = atoi(linia.c_str()); break;
+		}
+
+		nr_linii++;
+	}
+
+	plik.close();
+
+	cout << imie << endl;
+	cout << nazwisko  << endl;
+	cout << nr_tel  << endl;
+
+	return 0;
+}
